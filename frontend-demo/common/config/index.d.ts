3662ff607127c1b96d63de019eafd1ac995ecbeb
M. Daeva <embedmad@gmail.com>
2023-11-17 18:07:48 +0300

    removed frontend-demo

--- frontend-demo/common/config/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { NetworkConfig } from "../../common/interfaces";
-declare const MINTER_WASM = "minter.wasm";
-declare const STAKING_PLATFORM_WASM = "staking_platform.wasm";
-declare const NETWORK_CONFIG: NetworkConfig;
-export { NETWORK_CONFIG, MINTER_WASM, STAKING_PLATFORM_WASM };
--- frontend-demo/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import { nodePolyfills } from "@bangjelkoski/vite-plugin-node-polyfills";
-
-export default defineConfig({
-  plugins: [nodePolyfills({ protocolImports: true })],
-  resolve: {
-    preserveSymlinks: true,
-  },
-});
