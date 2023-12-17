d2a5122b9c9b3f01d976a9c1773c96ba11135c25
M. Daeva <embedmad@gmail.com>
2023-12-17 17:28:22 +0300

    added gov script

---
+++ contracts/scripts/gov/description.md
@@ -0,0 +1,9 @@
+### CryptoGopniks NFT Staking Platform: Minter Contract v.1.1.1
+
+CryptoGopniks team presents to your attention new gamechanging NFT staking platform! The app contains 2 smart contracts - `Minter` and `Staking Platform` by itself. The first one is designed for minting native tokens under the control of the platform. This proposal will allow to upload the `Minter` contract code v1.1.1 to the Stargaze network.
+
+Twitter link: https://twitter.com/StarGops_nft
+
+Link to discord: https://discord.gg/RuMJ54c2Yz
+
+Link to Telegram: https://t.me/CryptoGopnik_ru
---
+++ contracts/scripts/gov/gov-stargaze.sh
@@ -0,0 +1,38 @@
+DAEMON="starsd"
+# update for your config
+PASSWORD="12345678"
+
+# update for mainnet
+RPC="https://rpc.elgafar-1.stargaze-apis.com:443"
+CHAIN_ID="elgafar-1"
+
+TXFLAG="-y --gas auto --gas-prices 1ustars --gas-adjustment 1.3 --chain-id $CHAIN_ID --node $RPC"
+
+# specify your account address
+PROPOSAL_CREATOR="stars1f37v0rdvrred27tlqqcpkrqpzfv6ddr2a97zzu"
+# don't change!
+ADMIN="stars1f37v0rdvrred27tlqqcpkrqpzfv6ddr2a97zzu"
+
+MINTER_WASM="./minter.wasm"
+STAKING_PLATFORM_WASM="./staking_platform.wasm"
+
+TITLE="CryptoGopniks NFT Staking Platform: Minter Contract v.1.1.1"
+DESCRIPTION=$(cat description.md | jq -Rsa | tr -d '"')
+# set 50000000000 for mainnet
+DEPOSIT="1000000000ustars"
+
+
+# echo 'enter seed phrase'
+# echo "then enter keyring password twice"
+# $DAEMON keys add "admin-gopstake" --recover
+yes $PASSWORD | $DAEMON tx wasm submit-proposal wasm-store $MINTER_WASM \
+  --title "$TITLE" \
+  --summary "$DESCRIPTION" \
+  --authority $ADMIN \
+  --deposit $DEPOSIT \
+  --from $PROPOSAL_CREATOR \
+  --instantiate-anyof-addresses $ADMIN \
+  $TXFLAG
+
+
+
