4fb791c7820f3fde039ec8ab67bb2560babefdca
M. Daeva <embedmad@gmail.com>
2023-12-18 21:59:03 +0300

    added license

--- contracts/scripts/gov/gov-stargaze.sh
+++ contracts/scripts/gov/gov-stargaze.sh
@@ -13,26 +13,28 @@ PROPOSAL_CREATOR="stars1f37v0rdvrred27tlqqcpkrqpzfv6ddr2a97zzu"
 # don't change!
 ADMIN="stars1f37v0rdvrred27tlqqcpkrqpzfv6ddr2a97zzu"
 
-MINTER_WASM="./minter.wasm"
-STAKING_PLATFORM_WASM="./staking_platform.wasm"
+MINTER_WASM="../../artifacts/minter.wasm"
+STAKING_PLATFORM_WASM="../../artifacts/staking_platform.wasm"
 
 TITLE="CryptoGopniks NFT Staking Platform: Minter Contract v.1.1.1"
 DESCRIPTION=$(cat description.md | jq -Rsa | tr -d '"')
 # set 50000000000 for mainnet
 DEPOSIT="1000000000ustars"
 
+yes $PASSWORD | $DAEMON tx gov submit-proposal "./prop.json" --from "stars1f37v0rdvrred27tlqqcpkrqpzfv6ddr2a97zzu" $TXFLAG
+
 
 # echo 'enter seed phrase'
 # echo "then enter keyring password twice"
 # $DAEMON keys add "admin-gopstake" --recover
-yes $PASSWORD | $DAEMON tx wasm submit-proposal wasm-store $MINTER_WASM \
-  --title "$TITLE" \
-  --summary "$DESCRIPTION" \
-  --authority $ADMIN \
-  --deposit $DEPOSIT \
-  --from $PROPOSAL_CREATOR \
-  --instantiate-anyof-addresses $ADMIN \
-  $TXFLAG
+# yes $PASSWORD | $DAEMON tx wasm submit-proposal wasm-store $MINTER_WASM \
+#   --title "$TITLE" \
+#   --summary "$DESCRIPTION" \
+#   --authority $ADMIN \
+#   --deposit $DEPOSIT \
+#   --from $PROPOSAL_CREATOR \
+#   --instantiate-anyof-addresses $ADMIN \
+#   $TXFLAG
 
 
 
